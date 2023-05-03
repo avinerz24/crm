aacc1d926bec18da8e07f0c5b97c05bfaa97e18a
Avina Maldonado <avina_maldonado@hotmail.com>
2023-05-03 12:41:15 -0700

    Password Reset Email

--- crm/settings.py
+++ crm/settings.py
@@ -134,3 +134,12 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USER_TLS = True
+EMAIL_HOST_USER = 'avinaskates@gmail.com'
+EMAIL_HOST_PASSWORD = 'your app password'
