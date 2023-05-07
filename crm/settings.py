1eb2aab018805e1eac723864be1c94a9d7ebf82d
Avina Maldonado <avina_maldonado@hotmail.com>
2023-05-07 13:12:44 -0700

    Postgres Database

--- crm/settings.py
+++ crm/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'cvNxaBCicjYFGG6IlDHG',
+        'HOST': 'containers-us-west-46.railway.app',
+        'PORT': '6004',
     }
 }
 
